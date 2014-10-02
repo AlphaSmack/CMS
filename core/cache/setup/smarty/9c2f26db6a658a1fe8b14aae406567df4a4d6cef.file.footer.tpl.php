dd9fd38cc237c5b5ea4706a44a27a16b57621ee1
AlphaSmack <mrtp1988@gmail.com>
2014-10-03 01:08:00 +0300

    Adding template

---
+++ core/cache/setup/smarty/9c2f26db6a658a1fe8b14aae406567df4a4d6cef.file.footer.tpl.php
@@ -0,0 +1,43 @@
+<?php /* Smarty version Smarty-3.0.4, created on 2014-10-02 23:35:31
+         compiled from "C:/xampp/htdocs/CMS/setup/templates/footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:13849542dc5236b6c00-65041309%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '9c2f26db6a658a1fe8b14aae406567df4a4d6cef' => 
+    array (
+      0 => 'C:/xampp/htdocs/CMS/setup/templates/footer.tpl',
+      1 => 1406032582,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '13849542dc5236b6c00-65041309',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+        </div>
+        <!-- end content -->
+        <div class="clear">&nbsp;</div>
+    </div>
+</div>
+
+<!-- start footer -->
+<div id="footer">
+    <div id="footer-inner">
+    <div class="container_12">
+        <p><?php echo (isset($_smarty_tpl->getVariable('_lang')->value['modx_footer1']) ? $_smarty_tpl->getVariable('_lang')->value['modx_footer1'] : null);?>
+</p>
+        <p><?php echo (isset($_smarty_tpl->getVariable('_lang')->value['modx_footer2']) ? $_smarty_tpl->getVariable('_lang')->value['modx_footer2'] : null);?>
+</p>
+    </div>
+    </div>
+</div>
+
+<div class="post_body">
+
+</div>
+<!-- end footer -->
+</body>
+</html>
